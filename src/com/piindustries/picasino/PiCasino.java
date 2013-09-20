23a5018410c913c5e0a59e6dfc97fa7174f3fe9c
GitPS <sime.phil@uwlax.edu>
2013-09-20 13:29:16 -0500

    Added a main method so you can see how it compiles and runs with IntelliJ.

--- src/com/piindustries/picasino/PiCasino.java
+++ src/com/piindustries/picasino/PiCasino.java
@@ -1,10 +1,14 @@
 package com.piindustries.picasino;
 
 /**
- * User: Phil
  * Date: 9/20/13
  * Time: 1:18 PM
  */
 
 public class PiCasino {
+
+    public static void main(String[] args){
+        System.out.println("This is a test line to let you compile and see that it is working!");
+    }
+
 }
