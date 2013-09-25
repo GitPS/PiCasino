d7730bce9d34468e55b65ce59a620b40ce264538
GitPS <sime.phil@uwlax.edu>
2013-09-25 17:40:39 -0500

    Cleanup the master branch.  Please don't push any changes to this branch until we can all agree on the structure.

--- src/com/piindustries/picasino/PiCasino.java
+++ src/com/piindustries/picasino/PiCasino.java
@@ -1,8 +1,5 @@
 package com.piindustries.picasino;
 
-import com.piindustries.picasino.server_net.*;
-import com.piindustries.picasino.Cards.*;
-
 /**
  * Date: 9/20/13
  * Time: 1:18 PM
@@ -11,27 +8,6 @@ import com.piindustries.picasino.Cards.*;
 public class PiCasino {
 
     public static void main(String[] args){
-        PiCasino pc = new PiCasino();
-    }
-
-
-    public PiCasino(){
-        java.util.ArrayList<Card> cardList = new java.util.ArrayList<Card>();
-        for(int i = 0; i < 15; i++){
-            try{
-                cardList.add(new Heart(i));
-            }
-            catch(IllegalArgumentException a){
-                System.err.println(a.toString());
-            }
-        }
-
-        for(int i = 0; i < cardList.size(); i++){
-            Heart temp = null;
-            if(cardList.get(i) instanceof Heart){
-                temp = (Heart)cardList.get(i);
-            }
-            System.out.println("This is a " + temp.getValue() + " of " + temp.getSuit());
-        }
+        // TODO
     }
 }
