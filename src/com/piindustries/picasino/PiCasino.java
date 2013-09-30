c0bbd19ec2f22487547a8ff1166035b8387f64fd
AJJ13 <jensen.aaro@gmail.com>
2013-09-29 20:56:26 -0500

    Revert "ADD FILE COMMENT"
    This reverts commit db8fa5ad881394af4f64b37c7690a70e4ea2263a.

--- src/com/piindustries/picasino/PiCasino.java
+++ src/com/piindustries/picasino/PiCasino.java
@@ -1,32 +1,10 @@
-/*
- * [Class]
- * [Current Version]
- * [Date last modified]
- *
- * Copyright 2013 - Michael Hoyt, Aaron Jensen, Andrew Reis, and Phillip Sime.
- *
- * This file is part of PiCasino.
- *
- * PiCasino is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PiCasino is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PiCasino.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.piindustries.picasino;
 
 /**
  * Date: 9/20/13
  * Time: 1:18 PM
  */
+
 public class PiCasino {
 
     public static void main(String[] args){
