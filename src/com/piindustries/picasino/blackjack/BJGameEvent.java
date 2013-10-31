9a28237e83bdaeaf2a3752cee340c80fc4f2cfe2
Aaron James Jensen <jense_aj@i7clone011.local>
2013-10-30 20:36:50 -0500

    Created BJGameEvent Impl

---
+++ src/com/piindustries/picasino/blackjack/BJGameEvent.java
@@ -0,0 +1,63 @@
+/*
+ *
+ *       ___ _   ___          _
+ *      / _ (_) / __\__ _ ___(_)_ __   ___
+ *     / /_)/ |/ /  / _` / __| | '_ \ / _ \
+ *    / ___/| / /__| (_| \__ \ | | | | (_) |
+ *    \/    |_\____/\__,_|___/_|_| |_|\___/
+ *
+ *
+ * Class: com.piindustries.picasino.blackjack.BJGameEvent
+ * Version: 1.0
+ * Date: October 30, 2013
+ *
+ * Copyright 2013 - Michael Hoyt, Aaron Jensen, Andrew Reis, and Phillip Sime.
+ *
+ * This file is part of PiCasino.
+ *
+ * PiCasino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PiCasino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PiCasino.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.piindustries.picasino.blackjack;
+
+import com.piindustries.picasino.api.GameEvent;
+
+/**
+ * A basic BJGameEvent.  Unlike a BJServerGameEvent
+ * a BJGameEvent does not contain toUser information.
+ *
+ * @see com.piindustries.picasino.api.GameEvent
+ * @author A. Jensen
+ * @version 1.0
+ */
+public class BJGameEvent implements GameEvent {
+    private String name;
+    private Object value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
