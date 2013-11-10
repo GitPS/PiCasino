9ae70c0062217d14ed97eb6f8ad31a43e44f7c24
AJJ13 <jensen.aaro@gmail.com>
2013-11-10 09:22:57 -0600

    Extracted Inner Classes of BJClientGameState to Upper Level Classes
    This will make BJClientGameState lighter and easier to serialize,
    transmit, and, de-serialize.

--- src/com/piindustries/picasino/blackjack/BJGameEventType.java
+++ src/com/piindustries/picasino/blackjack/BJGameEventType.java
@@ -1,6 +1,35 @@
-package com.piindustries.picasino.blackjack;
+/*
+ *
+ *       ___ _   ___          _
+ *      / _ (_) / __\__ _ ___(_)_ __   ___
+ *     / /_)/ |/ /  / _` / __| | '_ \ / _ \
+ *    / ___/| / /__| (_| \__ \ | | | | (_) |
+ *    \/    |_\____/\__,_|___/_|_| |_|\___/
+ *
+ *
+ * Class: com.piindustries.picasino.blackjack.BJGameEventType
+ * Version: 1.0
+ * Date: November 9, 2013
+ *
+ * Copyright 2013 - Michael Hoyt, Aaron Jensen, Andrew Reis, and Phillip Sime.
+ *
+ * This file is part of PiCasino.
+ *
+ * PiCasino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PiCasino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PiCasino.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import java.util.ArrayList;
+package com.piindustries.picasino.blackjack;
 
 /**
  * Enumeration that contains all possible
