98d62a12e3708b339b565a49899a4e63c48c59dc
AJJ13 <jensen.aaro@gmail.com>
2013-11-10 09:23:10 -0600

    Extracted Inner Classes of BJClientGameState to Upper Level Classes
    This will make BJClientGameState lighter and easier to serialize,
    transmit, and, de-serialize.

--- src/com/piindustries/picasino/blackjack/BJHand.java
+++ src/com/piindustries/picasino/blackjack/BJHand.java
@@ -1,8 +1,16 @@
-package com.piindustries.picasino.blackjack;/*
- * [Class]
- * [Current Version]
- * [Date last modified]
- * 
+/*
+ *
+ *       ___ _   ___          _
+ *      / _ (_) / __\__ _ ___(_)_ __   ___
+ *     / /_)/ |/ /  / _` / __| | '_ \ / _ \
+ *    / ___/| / /__| (_| \__ \ | | | | (_) |
+ *    \/    |_\____/\__,_|___/_|_| |_|\___/
+ *
+ *
+ * Class: com.piindustries.picasino.blackjack.BJHand
+ * Version: 1.0
+ * Date: November 9, 2013
+ *
  * Copyright 2013 - Michael Hoyt, Aaron Jensen, Andrew Reis, and Phillip Sime.
  *
  * This file is part of PiCasino.
@@ -11,15 +19,17 @@ package com.piindustries.picasino.blackjack;/*
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * PiCasino is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with PiCasino.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+package com.piindustries.picasino.blackjack;
 
 import java.io.Serializable;
 import java.util.LinkedList;
