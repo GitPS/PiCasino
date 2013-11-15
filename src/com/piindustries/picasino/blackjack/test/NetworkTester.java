00370b40ddbadaaba2782a6fcb7dfab4b775a94e
GitPS <sime.phil@uwlax.edu>
2013-11-15 13:26:55 -0600

    Create skeleton NetworkTester class.

---
+++ src/com/piindustries/picasino/blackjack/test/NetworkTester.java
@@ -0,0 +1,34 @@
+package com.piindustries.picasino.blackjack.test;
+
+/**
+ * ___ _   ___          _
+ * / _ (_) / __\__ _ ___(_)_ __   ___
+ * / /_)/ |/ /  / _` / __| | '_ \ / _ \
+ * / ___/| / /__| (_| \__ \ | | | | (_) |
+ * \/    |_\____/\__,_|___/_|_| |_|\___/
+ * <p/>
+ * <p/>
+ * Class: com.piindustries.picasino.blackjack.test.NetworkTester
+ * Version: 1.0
+ * Date: 11/15/13
+ * <p/>
+ * Copyright 2013 - Michael Hoyt, Aaron Jensen, Andrew Reis, and Phillip Sime.
+ * <p/>
+ * This file is part of PiCasino.
+ * <p/>
+ * PiCasino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * PiCasino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with PiCasino.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class NetworkTester {
+}
