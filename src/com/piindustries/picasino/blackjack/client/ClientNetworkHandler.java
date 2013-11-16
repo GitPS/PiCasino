2c3b4a6d392f49af199bfc8e74849fb000872ec3
GitPS <sime.phil@uwlax.edu>
2013-11-15 20:39:26 -0600

    Update imports for Network class move.

--- src/com/piindustries/picasino/blackjack/client/ClientNetworkHandler.java
+++ src/com/piindustries/picasino/blackjack/client/ClientNetworkHandler.java
@@ -37,7 +37,7 @@ import com.esotericsoftware.kryonet.Listener;
 import com.piindustries.picasino.PiCasino;
 import com.piindustries.picasino.api.InvalidGameEventException;
 import com.piindustries.picasino.blackjack.domain.GameEvent;
-import com.piindustries.picasino.blackjack.test.Network;
+import com.piindustries.picasino.blackjack.domain.Network;
 
 import java.io.IOException;
 
