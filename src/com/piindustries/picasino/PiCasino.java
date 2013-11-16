0b9c51047f541f8922b61af8d20d025fd28ea19f
GitPS <sime.phil@uwlax.edu>
2013-11-16 12:08:20 -0600

    Remove unneeded comments.

--- src/com/piindustries/picasino/PiCasino.java
+++ src/com/piindustries/picasino/PiCasino.java
@@ -61,13 +61,11 @@ public class PiCasino {
     }
 
     private void buildServerBlackJack() {
-        /* We should only ever assign the GameState once. */
         networkHandler = new ServerNetworkHandler(this);
         gameState = new ServerGameState(this);
     }
 
     private void buildClientBlackJack(String host, String userName) {
-        /* We should only ever assign the GameState once. */
         networkHandler = new ClientNetworkHandler(this, host, userName);
         /* Pause for 2 seconds while the client establishes a connection. */
         try {
@@ -76,7 +74,6 @@ public class PiCasino {
             LOGGER.severe("Sleep was interrupted!");
             LOGGER.severe(e.getMessage());
         }
-
         gameState = new ClientGameState(this, userName);
     }
 
