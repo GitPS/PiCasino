d00c7e11e5d4b18f5825f8b0375f08193eb9a846
AJJ13 <jensen.aaro@gmail.com>
2013-11-20 19:28:03 -0600

    Slim down method

--- src/com/piindustries/picasino/blackjack/test/CardMaxValue.java
+++ src/com/piindustries/picasino/blackjack/test/CardMaxValue.java
@@ -8,9 +8,8 @@ public class CardMaxValue {
         for(int i =0; i < 20; i++ ){
             int[] deck = newDeck();
             int[] hand = new int[(int)(Math.random()*5) + 1];
-            for(int run = 0; run <  hand.length; run++){
+            for(int run = 0; run <  hand.length; run++)
                 hand[run] = deck[ (int)(Math.random()*52) ];
-            }
             StringBuilder sb = new StringBuilder();
             sb.append("Ids: [ ");
             for( int card: hand )
