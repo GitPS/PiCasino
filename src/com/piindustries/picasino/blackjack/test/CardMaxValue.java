e8ae6db885fb05dd6a5483e58f63207b46d2f0c1
AJJ13 <jensen.aaro@gmail.com>
2013-11-20 19:25:31 -0600

    Fix Test

--- src/com/piindustries/picasino/blackjack/test/CardMaxValue.java
+++ src/com/piindustries/picasino/blackjack/test/CardMaxValue.java
@@ -4,15 +4,18 @@ import com.piindustries.picasino.blackjack.domain.Cards;
 
 public class CardMaxValue {
 
-    public synchronized static void main(String[] args){
+    public static void main(String[] args){
         for(int i =0; i < 20; i++ ){
             int[] deck = newDeck();
             int[] hand = new int[(int)(Math.random()*5) + 1];
             for(int run = 0; run <  hand.length; run++){
-                hand[i] = deck[ (int)(Math.random()*52) ];
+                hand[run] = deck[ (int)(Math.random()*52) ];
             }
             StringBuilder sb = new StringBuilder();
-            sb.append("Hand: [ ");
+            sb.append("Ids: [ ");
+            for( int card: hand )
+                sb.append( card + " " );
+            sb.append("] Hand: [ ");
             for( int card : hand ){ sb.append(Cards.evaluateCardName(card) + " "); }
             sb.append("] Value: " + Cards.getMaxHandValue(hand));
             System.out.println( sb.toString() );
