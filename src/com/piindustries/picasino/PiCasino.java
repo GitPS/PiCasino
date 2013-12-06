612c07f1b488edeb3e721481238fdb0d77ca162c
GitPS <sime.phil@uwlax.edu>
2013-12-06 08:40:28 -0600

    Use full URL when downloading from Dropbox.

--- src/com/piindustries/picasino/PiCasino.java
+++ src/com/piindustries/picasino/PiCasino.java
@@ -23,7 +23,7 @@ public class PiCasino {
 
     /* Version */
     public static final String VERSION = "1.0";
-    public static final String UPDATE_URL = "https://db.tt/4X1O3MGs";
+    public static final String UPDATE_URL = "https://www.dropbox.com/s/dlyzzluw3tlh516/PiCasino.jar";
 
     /* Error messages */
     private static final String invalidArgsMsg = "Invalid or missing command line arguments.";
