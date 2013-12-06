7753e5a8575c12393a651fcccc79212cbdaee892
GitPS <sime.phil@uwlax.edu>
2013-12-05 20:23:14 -0600

    Handle User and Update objects from client.  This is still a work in progress.

--- src/com/piindustries/picasino/launcher/LauncherNetworkHandler.java
+++ src/com/piindustries/picasino/launcher/LauncherNetworkHandler.java
@@ -4,6 +4,7 @@ import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 import com.piindustries.picasino.PiCasino;
+import com.piindustries.picasino.blackjack.database.BlackjackDatabaseConnector;
 
 import java.io.IOException;
 
@@ -26,7 +27,19 @@ public class LauncherNetworkHandler {
             public void received(Connection connection, Object object) {
                 if (object instanceof User) {
                     User user = (User) object;
-                    PiCasino.LOGGER.info("Received a user object from the launcher.");
+                    /* Allow the called method to verify the user is authenticated */
+                    user.setAuthenticated(handleUser(user));
+                    /* Send the user back to the launcher */
+                    send(user, connection.getID());
+                }
+                else if(object instanceof Update){
+                    Update update = (Update)object;
+                    /* Check for update and set version and download URL for PiCasino.jar */
+                    update.setUpdateNeeded(update.getClientVersion().equalsIgnoreCase(PiCasino.VERSION));
+                    update.setServerVersion(PiCasino.VERSION);
+                    update.setUpdateURL(PiCasino.UPDATE_URL);
+                    /* Send the update information back to the launcher */
+                    send(update, connection.getID());
                 }
             }
 
@@ -48,5 +61,20 @@ public class LauncherNetworkHandler {
 
         /* Notify console that the server started and is waiting for connections */
         PiCasino.LOGGER.info("Launcher connection server is running and waiting for connections...");
+    }
+
+    private void send(Object o, int id){
+        server.sendToTCP(id, o);
+    }
+
+    private boolean handleUser(User user){
+        BlackjackDatabaseConnector db = new BlackjackDatabaseConnector();
+        /* Check if player exists already, if not create that user */
+        if(db.checkUserExist(user.getUsername())){
+            return db.checkPlayerLogin(user.getUsername(), user.getPassword());
+        }
+        else{
+            return db.createNewPlayer(user.getUsername(), user.getPassword(), user.getFirstName(), user.getLastName(), user.getEmail());
+        }
     }
 }
